35ad13027d9048af924a6cdfa4ab4daad3394b5d
Andrey Shulga <andreyshulga@coherentsolutions.com>
2024-02-11 15:13:40 +0200

    fix test issue

--- src/utils/addEventListenerAll.js
+++ src/utils/addEventListenerAll.js
@@ -1,7 +1,5 @@
 export default (elems, type, callback) => {
-    elems.forEach(elem => {
-        elem.addEventListener(type, callback)
-    });
+ elems.forEach((elem) => {
+  elem.addEventListener(type, callback);
+ });
 };
-// Useful comment
-
