9c7617bf01f2f1d6a83a153b08c7c95c9ea1e0ea
Andrey Shulga <andreyshulga@coherentsolutions.com>
2024-02-12 12:49:59 +0200

    refactoring

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 import express from "express";
-// import ViteExpress from "vite-express";
 import { fileURLToPath } from "url";
 import { dirname } from "path";
 
@@ -7,7 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const app = express();
 const PORT = 3000;
-// ViteExpress.listen(app, 3000, () => console.log("Server is listening..."));
+
 app.listen(PORT, function (err) {
  if (err) console.log("Error in server setup");
  console.log("Server listening on Port", PORT);
@@ -17,6 +16,6 @@ app.use("/assets", express.static(__dirname + "/dist/assets"));
 app.use("/img", express.static(__dirname + "/dist/img"));
 app.use("/scss", express.static(__dirname + "/dist/scss"));
 
-app.get("*", function (req, res) {
+app.get("*", function (_, res) {
  res.sendFile("dist/index.html", { root: __dirname });
 });
